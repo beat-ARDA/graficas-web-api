a6d3205798fc9db46418a1aff7d1a2dc80757650
beat-ARDA <alvaro_07051@outlook.com>
2023-05-26 14:50:26 -0600

    retorno

--- index.js
+++ index.js
@@ -13,85 +13,85 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// const socketIO = require('socket.io')(http, {
-//     cors: {
-//         origin: "*",
-//         methods: ["GET", "POST"]
-//     }
-// });
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 let users = [];
 
-// socketIO.on('connection', (socket) => {
+socketIO.on('connection', (socket) => {
 
-//     console.log(`⚡: ${socket.id} user just connected!`);
+    console.log(`⚡: ${socket.id} user just connected!`);
 
-//     users.push(socket.id);
+    users.push(socket.id);
 
-//     socketIO.emit('socketId', { "socketId": socket.id, "posUser": users.length });
+    socketIO.emit('socketId', { "socketId": socket.id, "posUser": users.length });
 
-//     if (users.length === 2)
-//         socketIO.emit('sessionReady');
+    if (users.length === 2)
+        socketIO.emit('sessionReady');
 
-//     socket.on('newUser', (infoUser) => {
-//         socketIO.emit('newUser', infoUser);
-//     });
+    socket.on('newUser', (infoUser) => {
+        socketIO.emit('newUser', infoUser);
+    });
 
-//     socket.on('moveLeft', (socketId) => {
-//         socketIO.emit('moveLeft', socketId);
-//     });
+    socket.on('moveLeft', (socketId) => {
+        socketIO.emit('moveLeft', socketId);
+    });
 
-//     socket.on('moveLeaveLeft', (socketId) => {
-//         console.log('🛅  moveLeaveLeft: ' + socketId);
-//         socketIO.emit('moveLeaveLeft', socketId);
-//     });
+    socket.on('moveLeaveLeft', (socketId) => {
+        console.log('🛅  moveLeaveLeft: ' + socketId);
+        socketIO.emit('moveLeaveLeft', socketId);
+    });
 
-//     socket.on('moveRight', (socketId) => {
-//         console.log('🤜 moveRight' + socketId);
-//         socketIO.emit('moveRight', socketId);
-//     });
+    socket.on('moveRight', (socketId) => {
+        console.log('🤜 moveRight' + socketId);
+        socketIO.emit('moveRight', socketId);
+    });
 
-//     socket.on('moveUp', (socketId) => {
-//         console.log('🔺  moveUp' + socketId);
-//         socketIO.emit('moveUp', socketId);
-//     });
+    socket.on('moveUp', (socketId) => {
+        console.log('🔺  moveUp' + socketId);
+        socketIO.emit('moveUp', socketId);
+    });
 
-//     socket.on('moveDown', (socketId) => {
-//         console.log('🔻  moveDown' + socketId);
-//         socketIO.emit('moveDown', socketId);
-//     });
+    socket.on('moveDown', (socketId) => {
+        console.log('🔻  moveDown' + socketId);
+        socketIO.emit('moveDown', socketId);
+    });
 
-//     socket.on('moveShoot', (socketId) => {
-//         console.log('🔻  moveShoot' + socketId);
-//         socketIO.emit('moveShoot', socketId);
-//     });
+    socket.on('moveShoot', (socketId) => {
+        console.log('🔻  moveShoot' + socketId);
+        socketIO.emit('moveShoot', socketId);
+    });
 
-//     socket.on('moveLeaveUp', (socketId) => {
-//         console.log('🔼  moveLeaveUp' + socketId);
-//         socketIO.emit('moveLeaveUp', socketId);
-//     });
+    socket.on('moveLeaveUp', (socketId) => {
+        console.log('🔼  moveLeaveUp' + socketId);
+        socketIO.emit('moveLeaveUp', socketId);
+    });
 
-//     socket.on('moveLeaveDown', (socketId) => {
-//         console.log('🔽  moveLeaveDown' + socketId);
-//         socketIO.emit('moveLeaveDown', socketId);
-//     });
+    socket.on('moveLeaveDown', (socketId) => {
+        console.log('🔽  moveLeaveDown' + socketId);
+        socketIO.emit('moveLeaveDown', socketId);
+    });
 
-//     socket.on('moveLeaveRight', (socketId) => {
-//         console.log('🔎  moveLeaveRight' + socketId);
-//         socketIO.emit('moveLeaveRight', socketId);
-//     });
+    socket.on('moveLeaveRight', (socketId) => {
+        console.log('🔎  moveLeaveRight' + socketId);
+        socketIO.emit('moveLeaveRight', socketId);
+    });
 
-//     socket.on('moveLeaveShoot', (socketId) => {
-//         console.log('🔎  moveLeaveShoot' + socketId);
-//         socketIO.emit('moveLeaveShoot', socketId);
-//     });
+    socket.on('moveLeaveShoot', (socketId) => {
+        console.log('🔎  moveLeaveShoot' + socketId);
+        socketIO.emit('moveLeaveShoot', socketId);
+    });
 
-//     socket.on('disconnect', () => {
-//         console.log('🔥: A user disconnected' + users);
-//         users.splice(users.indexOf(socket.id), 1);
-//         socket.disconnect();
-//     });
-// });
+    socket.on('disconnect', () => {
+        console.log('🔥: A user disconnected' + users);
+        users.splice(users.indexOf(socket.id), 1);
+        socket.disconnect();
+    });
+});
 
 app.use('/api', onePlayerRoutes);
 app.use('/api', twoPlayerRoutes);
